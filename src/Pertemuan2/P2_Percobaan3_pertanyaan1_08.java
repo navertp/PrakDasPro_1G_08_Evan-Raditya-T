d513c7262a7d5aeeab2095a8c1c1840ebe4b733f
Evan Raditya <evanraditya0608@gmail.com>
2024-09-12 21:14:56 +0700

    KRS Evan

---
+++ src/Pertemuan2/P2_Percobaan3_pertanyaan1_08.java
@@ -0,0 +1,29 @@
+package Pertemuan2;
+
+public class P2_Percobaan3_pertanyaan1_08 {
+
+    public static void main(String[] args) {
+        System.out.println("                                                                               ");
+        System.out.println("                            KARTU RENCANA STUDI (KRS)                          ");
+        System.out.println("                                                                               ");
+        System.out.println("NIM            : 244107020103");
+        System.out.println("NAMA           : EVAN RADITYA TARUNA PUTRA");
+        System.out.println("PROGRAM STUDI  : Diploma IV Teknik Informatika");
+        System.out.println("KELAS          : 1G");
+        System.out.println("                                                                               ");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("|    KODE MK   |                   MATA KULIAH                    | SKS | JAM |");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("|   RT1241001  | Pancasila                                        |  2  |  2  |");
+        System.out.println("|   RT1241002  | Konsep Teknologi Informasi                       |  2  |  4  |");
+        System.out.println("|   RT1241003  | Critical Thinking dan Problem Solving            |  2  |  4  |");
+        System.out.println("|   RT1241004  | Matematika Dasar                                 |  3  |  6  |");
+        System.out.println("|   RT1241005  | Rekayasa Perangkat Lunak                         |  2  |  4  |");
+        System.out.println("|   RT1241006  | Dasar Pemrograman                                |  2  |  4  |");
+        System.out.println("|   RT1241007  | Praktikum Dasar Pemrograman                      |  3  |  6  |");
+        System.out.println("|   RT1241008  | Keselamatan dan Kesehatan Kerja                  |  2  |  4  |");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("|              |                      JUMLAH                      | 18  | 34  |");
+        System.out.println("-------------------------------------------------------------------------------");
+    }
+}

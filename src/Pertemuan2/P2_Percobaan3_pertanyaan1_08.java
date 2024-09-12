5a3a05e2faa021e0c3ea18e5f659a4b73750e343
Evan Raditya <evanraditya0608@gmail.com>
2024-09-12 21:26:52 +0700

    Identitas Evan

---
+++ src/Pertemuan2/P2_Percobaan3_pertanyaan1_08.java
@@ -0,0 +1,18 @@
+package Pertemuan2;
+
+public class P2_Percobaan3_pertanyaan1_08 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Evan Raditya Taruna Putra");
+        System.out.println("NIM     : 244107020103");
+        System.out.println("Kelas   : TI 1G");
+        System.out.println("No Absen: 08");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Jl. D. Tondano Barat A2-B19, Sawojajar, Malang");
+        System.out.println("No HP   : 085707019845");
+        System.out.println("Hobi    : Berenang");
+    }
+}

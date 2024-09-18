373b8b9e442bb3d20e2c3c56556b51baa3d312ca
Evan Raditya <evanraditya0608@gmail.com>
2024-09-18 23:42:31 +0700

    Tugas1_P3

---
+++ src/Pertemuan3/TagihanListrik08.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TagihanListrik08 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        String namaPelanggan;
+        double tarif = 1500.0;
+        int penggunaan;
+
+        System.out.println("Aplikasi Tagihan Listrik");
+
+        System.out.println("");
+        System.out.print("Masukkan nama pelanggan : ");
+        namaPelanggan = input.nextLine();
+
+        System.out.print("Masukkan penggunaan listrik : ");
+        penggunaan = input.nextInt();
+
+        double totalHarga = penggunaan * tarif;
+
+        System.out.println("");
+        System.out.println("Nama pelanggan : " + namaPelanggan);
+        System.out.println("Total Tagihan : " + totalHarga);
+
+        boolean check = penggunaan > 500;
+        System.out.println("Apakah penggunaan listrik anda melebihi 500 kWh? " + check);
+        }
+}

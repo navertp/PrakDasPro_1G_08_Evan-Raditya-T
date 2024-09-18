4eb02b6217a9362020a70c025d58c790f0b9dcef
Evan Raditya <evanraditya0608@gmail.com>
2024-09-18 22:52:44 +0700

    P3_Percobaan2_PertanyaanNo2

--- src/Pertemuan3/Kafe08.java
+++ src/Pertemuan3/Kafe08.java
@@ -6,7 +6,7 @@ public class Kafe08 {
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
-        float diskon = 10 / 100f;
+        float diskon = 10 / 100;
 
         System.out.print("Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();

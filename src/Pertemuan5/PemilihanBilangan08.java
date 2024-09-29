45a07be5c1716527985c62349c2c4c105580f806
Evan Raditya <evanraditya0608@gmail.com>
2024-09-29 13:52:05 +0700

    P5_Percobaan1_PertanyaanNo1

--- src/Pertemuan5/PemilihanBilangan08.java
+++ src/Pertemuan5/PemilihanBilangan08.java
@@ -8,13 +8,9 @@ public class PemilihanBilangan08 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+        
+        System.out.println("Angka " + angka + " termasuk bilangan " + hasil);
         }
     }
-}
+

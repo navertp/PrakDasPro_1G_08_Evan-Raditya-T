bd02e78879f6cad4822e99afefb429ad53130aca
Evan Raditya <evanraditya0608@gmail.com>
2024-10-11 14:45:58 +0700

    P7_Percobaan1_Pertanyaan5

--- src/Pertemuan7/SiakadFor08.java
+++ src/Pertemuan7/SiakadFor08.java
@@ -7,6 +7,7 @@ public class SiakadFor08 {
         Scanner sc = new Scanner (System.in);
 
         double nilai08, tertinggi08 = 0, terendah08 = 100;
+        int batasLulus08 = 60, mahasiswaLulus  = 0, mahasiswaTidaklulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
@@ -17,9 +18,15 @@ public class SiakadFor08 {
             if (nilai08 < terendah08) {
                 terendah08 = nilai08;
             }
+            if (nilai08 >= batasLulus08) {
+            mahasiswaLulus++;
+            } else {
+            mahasiswaTidaklulus++;
+            }
         }
         System.out.println("Nilai Tertinggi : " + tertinggi08);
         System.out.println("Nilai Terendah : " + terendah08);
+        System.out.println("Jumlah Mahasiswa Lulus : " + mahasiswaLulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus : " + mahasiswaTidaklulus);
     }
 }
-

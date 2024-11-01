c2ff06341b99041a864cdd888a377673063931a3
Evan Raditya <evanraditya0608@gmail.com>
2024-11-01 14:40:49 +0700

    P10_Percobaan1_PertanyaanNo4

--- src/Pertemuan10/ArrayBilangan08.java
+++ src/Pertemuan10/ArrayBilangan08.java
@@ -4,7 +4,7 @@ public class ArrayBilangan08 {
     public static void main(String[] args) {
         int[] bil = {5, 13, -7, 17};
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= 4; i++) {
             System.out.println(bil[i]);
         }
     }

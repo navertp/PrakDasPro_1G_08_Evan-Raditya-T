e674f9e8f84d4069aa20e58d8b5118b25d5253ee
Evan Raditya <evanraditya0608@gmail.com>
2024-11-01 14:38:59 +0700

    P10_Percobaan1_PertanyaanNo2

--- src/Pertemuan10/ArrayBilangan08.java
+++ src/Pertemuan10/ArrayBilangan08.java
@@ -2,12 +2,7 @@ package Pertemuan10;
 
 public class ArrayBilangan08 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
-
-        bil[0] = 5.0;
-        bil[1] = 12867;
-        bil[2] = 7.5;
-        bil[3] = 2000000;
+        int[] bil = {5, 13, -7, 17};
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);

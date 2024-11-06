868f1383ee66b78c964c969471ecb93c49054617
Evan Raditya <evanraditya0608@gmail.com>
2024-11-06 09:24:15 +0700

    P10_Percobaan2_PertanyaanNo1

--- src/Pertemuan10/ArrayNilai08.java
+++ src/Pertemuan10/ArrayNilai08.java
@@ -9,7 +9,7 @@ public class ArrayNilai08 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan Nilai Akhir Ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }

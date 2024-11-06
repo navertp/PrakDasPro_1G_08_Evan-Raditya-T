6e9bda62bf025179c3bb6b4e3ad72ee5a1d980f9
Evan Raditya <evanraditya0608@gmail.com>
2024-11-06 09:28:47 +0700

    P10_Percobaan2_PertanyaanNo3

--- src/Pertemuan10/ArrayNilai08.java
+++ src/Pertemuan10/ArrayNilai08.java
@@ -13,8 +13,10 @@ public class ArrayNilai08 {
             System.out.print("Masukkan Nilai Akhir Ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai Akhir Ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa Ke-" + i + " lulus!");
+            }
         }
     }
 }

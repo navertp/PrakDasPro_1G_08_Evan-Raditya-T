f583bfe311fbfebf8542a86ad5a2a92276ec766f
Evan Raditya <evanraditya0608@gmail.com>
2024-11-07 00:43:07 +0700

    P10_Percobaan3_PertanyaanNo2

--- src/Pertemuan10/ArrayRataNilai08.java
+++ src/Pertemuan10/ArrayRataNilai08.java
@@ -6,10 +6,13 @@ public class ArrayRataNilai08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        int[] nilaiMhs= new int[10];
-        int mhsLulus = 0;
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+
+        int[] nilaiMhs= new int[jumlahMhs];
+        int mhsLulus = 0, mhsTidakLulus = 0;
         double total = 0;
-        double rata2;
+        double rata2, rata2Lulus = 0, rata2TidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan Nilai Mahasiswa Ke-" + (i+1) + " : ");
@@ -23,10 +26,20 @@ public class ArrayRataNilai08 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             if (nilaiMhs[i] > 70) {
                 mhsLulus++;
+                rata2Lulus += nilaiMhs[i];
+            } else {
+                mhsTidakLulus++;
+                rata2TidakLulus += nilaiMhs[i];
             }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata - Rata Nilai = " + rata2);
-        System.out.println("Jumlah Mahasiswa Yang Lulus Sebanyak " + mhsLulus + " Mahasiswa");
+        rata2Lulus = rata2Lulus / mhsLulus;
+        rata2TidakLulus = rata2TidakLulus / mhsTidakLulus;
+
+        System.out.println("Rata-rata Nilai Lulus = " + rata2Lulus);
+        System.out.println("Rata-rata Nilai Tidak lulus = " + rata2TidakLulus);
+
+        //rata2 = total/nilaiMhs.length;
+        //System.out.println("Rata - Rata Nilai = " + rata2);
+        //System.out.println("Jumlah Mahasiswa Yang Lulus Sebanyak " + mhsLulus + " Mahasiswa");
     }
 }

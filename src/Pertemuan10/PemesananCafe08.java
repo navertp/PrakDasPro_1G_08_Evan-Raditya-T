cf882b07e7b8ee4cd1344e76ee2b57cdd0b45b5d
Evan Raditya <evanraditya0608@gmail.com>
2024-11-07 20:29:30 +0700

    P10_Tugas2

---
+++ src/Pertemuan10/PemesananCafe08.java
@@ -0,0 +1,35 @@
+package Pertemuan10;
+
+import java.util.Scanner;
+
+public class PemesananCafe08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah pesanan: ");
+        int jumlahPesanan = sc.nextInt();
+        sc.nextLine();
+
+        String[] namaPesanan = new String[jumlahPesanan];
+        int[] hargaPesanan = new int[jumlahPesanan]; 
+        int totalBiaya = 0;
+
+        for (int i = 0; i < jumlahPesanan; i++) {
+            System.out.print("Masukkan nama pesanan ke-" + (i + 1) + ": ");
+            namaPesanan[i] = sc.nextLine();
+
+            System.out.print("Masukkan harga untuk " + namaPesanan[i] + ": ");
+            hargaPesanan[i] = sc.nextInt();
+            sc.nextLine();
+
+            totalBiaya += hargaPesanan[i];
+        }
+
+        System.out.println();
+        System.out.println("Daftar Pesanan:");
+        for (int i = 0; i < jumlahPesanan; i++) {
+            System.out.println((i + 1) + ". " + namaPesanan[i] + " - Rp " + hargaPesanan[i]);
+        }
+        System.out.println("Total Biaya: Rp " + totalBiaya);
+    }
+}   

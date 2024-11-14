1a55f5b2a1c4d0813942cf38f471b07a7757294a
Evan Raditya <evanraditya0608@gmail.com>
2024-11-14 22:14:29 +0700

    P11_Percobaan4

---
+++ src/Pertemuan11/SIAKAD08.java
@@ -0,0 +1,36 @@
+package Pertemuan11;
+
+import java.util.Scanner;
+
+public class SIAKAD08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] nilai = new int[4][3];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Input Nilai Mahasiswa Ke-" + (i+1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai Mata Kuliah " + (j+1) + " : ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai Rata-Rata : " + totalPerSiswa/3);
+        }
+        System.out.println("\n================================");
+        System.out.println("Rata-Rata Nilai Setiap Mata Kuliah : ");
+
+        for (int j = 0; j < 3; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < 4; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+            
+            System.out.println("Mata Kuliah " + (j+1) + ": " + totalPerMatkul / 4);
+        }
+    }
+}

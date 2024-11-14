7aae625a5509f07369a28f3fc8112817d512b617
Evan Raditya <evanraditya0608@gmail.com>
2024-11-14 21:22:27 +0700

    P11_Percobaan3

---
+++ src/Pertemuan11/Numbers08.java
@@ -0,0 +1,12 @@
+package Pertemuan11;
+
+public class Numbers08 {
+    public static void main(String[] args) {
+
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+        
+    }
+}

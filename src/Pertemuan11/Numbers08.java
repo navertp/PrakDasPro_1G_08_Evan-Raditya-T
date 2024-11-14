67debd0a0f6a7f2ad2858e671d1c87b51f23af6d
Evan Raditya <evanraditya0608@gmail.com>
2024-11-14 21:23:06 +0700

    P11_Percobaan3_PertanyaanNo1

--- src/Pertemuan11/Numbers08.java
+++ src/Pertemuan11/Numbers08.java
@@ -1,5 +1,7 @@
 package Pertemuan11;
 
+import java.util.Arrays;
+
 public class Numbers08 {
     public static void main(String[] args) {
 
@@ -7,6 +9,9 @@ public class Numbers08 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
-        
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }

d486fcd1871a598dda8ec1bfe5f1542fcad3f906
Evan Raditya <evanraditya0608@gmail.com>
2024-11-29 13:51:09 +0700

    P13_Percobaan1_PertanyaanNo2

--- src/Pertemuan13/Kafe08.java
+++ src/Pertemuan13/Kafe08.java
@@ -1,7 +1,7 @@
 package Pertemuan13;
 
 public class Kafe08 {
-    public static void Menu() {
+    public static void main(String[] args) {
         System.out.println("===== MENU RESTO CAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -11,8 +11,5 @@ public class Kafe08 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("============================");
         System.out.println("Silahkan Pilih Menu yang Anda Inginkan.");
-    }
-    public static void main(String[] args) {
-        Menu();
     }
 }

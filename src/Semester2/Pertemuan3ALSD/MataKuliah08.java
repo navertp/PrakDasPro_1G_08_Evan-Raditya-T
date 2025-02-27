d1358673dbbc9ab8da9b191d16f43991dd181c0a
navertp <evanraditya0608@gmail.com>
2025-02-27 10:48:01 +0700

    P3Sem2_Percobaan3

--- src/Semester2/Pertemuan3ALSD/MataKuliah08.java
+++ src/Semester2/Pertemuan3ALSD/MataKuliah08.java
@@ -1,7 +1,5 @@
 package Semester2.Pertemuan3ALSD;
 
-import java.util.Scanner;
-
 public class MataKuliah08 {
     public String kode;
     public String nama;
@@ -13,17 +11,5 @@ public class MataKuliah08 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
-    }
-    
-    public void tambahData(Scanner sc) {
-        System.out.print("Kode       : ");
-        this.kode = sc.nextLine();
-        System.out.print("Nama       : ");
-        this.nama = sc.nextLine();
-        System.out.print("SKS        : ");
-        this.sks = Integer.parseInt(sc.nextLine());
-        System.out.print("Jumlah Jam : ");
-        this.jumlahJam = Integer.parseInt(sc.nextLine());
-        System.out.println("-----------------------------");
     }
 }

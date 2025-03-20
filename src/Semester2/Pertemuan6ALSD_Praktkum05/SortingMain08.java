62bbfa3eb475883f446be624c0bb78a00ca70463
navertp <evanraditya0608@gmail.com>
2025-03-20 08:46:13 +0700

    P6Sem2_SortingSelectionSort

--- src/Semester2/Pertemuan6ALSD_Praktkum05/SortingMain08.java
+++ src/Semester2/Pertemuan6ALSD_Praktkum05/SortingMain08.java
@@ -11,5 +11,15 @@ public class SortingMain08 {
         dataurut1.bubbleSort();
         System.out.println("Data Sudah Diurutkan Dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        int b[] = {30, 20, 2, 8, 14};
+
+        Sorting08 dataurut2 = new Sorting08(b, b.length);
+
+        System.out.println("Data Awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data Sudah Diurutkan Dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }

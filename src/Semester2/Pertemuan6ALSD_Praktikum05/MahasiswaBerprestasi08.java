b3a83a85f58ae6904bfa79490acaf78dd207eb50
navertp <evanraditya0608@gmail.com>
2025-03-27 11:45:06 +0700

    P7Sem2_Percobaan1

--- src/Semester2/Pertemuan6ALSD_Praktikum05/MahasiswaBerprestasi08.java
+++ src/Semester2/Pertemuan6ALSD_Praktikum05/MahasiswaBerprestasi08.java
@@ -65,4 +65,34 @@ public class MahasiswaBerprestasi08 {
             listMhs[j] = temp;
         }
     }
+
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk==cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos!=-1) {
+            System.out.println("Data Mahasiswa dengan IPK : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("Data " + x + " tidak ditemukan");
+        }
+    }
+    
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("NIM\t : " + listMhs[pos].nim);
+            System.out.println("Nama\t : " + listMhs[pos].nama);
+            System.out.println("Kelas\t : " + listMhs[pos].kelas);
+            System.out.println("IPK\t : " + listMhs[pos].ipk);
+        } else {
+            System.out.println("Data Mahasiswa Dengan IPK " + x + " tidak ditemukans");
+        }
+    }
 }
--- src/Semester2/Pertemuan6ALSD_Praktikum05/MahasiswaDemo08.java
+++ src/Semester2/Pertemuan6ALSD_Praktikum05/MahasiswaDemo08.java
@@ -7,10 +7,9 @@ public class MahasiswaDemo08 {
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = sc.nextInt();
-        sc.nextLine();
-        MahasiswaBerprestasi08 list = new MahasiswaBerprestasi08(jumlah);
-        for (int i = 0; i < jumlah; i++) {
+        int jumMhs = 5;
+        MahasiswaBerprestasi08 list = new MahasiswaBerprestasi08();
+        for (int i = 0; i < jumMhs; i++) {
             System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
             System.out.print("NIM: ");
             String nim = sc.nextLine();
@@ -25,20 +24,34 @@ public class MahasiswaDemo08 {
             Mahasiswa08 m = new Mahasiswa08(nim, nama, kelas, ipk);
             list.tambah(m);
         }
+            
+            list.tampil();
+            System.out.println("--------------------------------------");
+            System.out.println("Pencarian Data");
+            System.out.println("--------------------------------------");
+            System.out.println("masukkan ipk mahasiswa yang dicari: ");
+            System.out.print("IPK: ");
+            double cari = sc.nextDouble();
+            
+            System.out.println("menggunakan sequential searching");
+            double posisi = list.sequentialSearching(cari);
+            int pss= (int)posisi;
+            list. tampilPosisi(cari, pss);
+            list.tampilDataSearch(cari, pss);
         
-        System.out.println("\nData mahasiswa sebelum sorting: ");
-        list.tampil();
+        // System.out.println("\nData mahasiswa sebelum sorting: ");
+        // list.tampil();
         
-        System.out.println("\nData Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
-        list.bubbleSort();
-        list.tampil();
+        // System.out.println("\nData Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
+        // list.bubbleSort();
+        // list.tampil();
         
-        System.out.println("Data Yang Sudah Terurut Menggunakan SELECTION SORT (ASC)");
-        list.selectionSort();
-        list.tampil();
+        // System.out.println("Data Yang Sudah Terurut Menggunakan SELECTION SORT (ASC)");
+        // list.selectionSort();
+        // list.tampil();
 
-        System.out.println("Data Yang Sudah Terurut Menggunakan INSERTION SORT (DESC)");
-        list.insertionSort();
-        list.tampil();
+        // System.out.println("Data Yang Sudah Terurut Menggunakan INSERTION SORT (DESC)");
+        // list.insertionSort();
+        // list.tampil();
     }
 }

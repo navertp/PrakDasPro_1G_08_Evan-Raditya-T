6cd8f508a99717139143bd0951842c084c5cbd4a
navertp <evanraditya0608@gmail.com>
2025-05-15 09:51:02 +0700

    P12Sem2_Percobaan1_Pertanyaam3

--- src/Semester2/Pertemuan12ALSD_Jobsheet11/SLLMain08.java
+++ src/Semester2/Pertemuan12ALSD_Jobsheet11/SLLMain08.java
@@ -1,13 +1,34 @@
 package Semester2.Pertemuan12ALSD_Jobsheet11;
 
+import java.util.Scanner;
+
 public class SLLMain08 {
+    public static Mahasiswa08 inputMahasiswa(Scanner sc) {
+        System.out.print("NIM   : ");
+        String nim = sc.nextLine();
+        System.out.print("Nama  : ");
+        String nama = sc.nextLine();
+        System.out.print("Kelas : ");
+        String kelas = sc.nextLine();
+        System.out.print("IPK   : ");
+        double ipk = sc.nextDouble();
+        sc.nextLine();
+        return new Mahasiswa08(nim, nama, kelas, ipk);
+    } 
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList08 sll = new SingleLinkedList08();
 
-        Mahasiswa08 mhs1 = new Mahasiswa08("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa08 mhs2 = new Mahasiswa08("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa08 mhs3 = new Mahasiswa08("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa08 mhs4 = new Mahasiswa08("21212203", "Dirga", "4D", 3.6);
+        System.out.println("Masukkan data 4 Mahasiswa:");
+        System.out.println("Data untuk mhs1:");
+        Mahasiswa08 mhs1 = inputMahasiswa(sc);
+        System.out.println("Data untuk mhs2:");
+        Mahasiswa08 mhs2 = inputMahasiswa(sc);
+        System.out.println("Data untuk mhs3:");
+        Mahasiswa08 mhs3 = inputMahasiswa(sc);
+        System.out.println("Data untuk mhs4:");
+        Mahasiswa08 mhs4 = inputMahasiswa(sc);
 
         sll.print();
         sll.addFirst(mhs4);

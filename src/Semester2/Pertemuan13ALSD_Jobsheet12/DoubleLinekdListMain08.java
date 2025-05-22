c382b3792f1beca7788e934182b310d8b936326c
navertp <evanraditya0608@gmail.com>
2025-05-22 11:13:53 +0700

    P13Sem2_Percobaan3

--- src/Semester2/Pertemuan13ALSD_Jobsheet12/DoubleLinekdListMain08.java
+++ src/Semester2/Pertemuan13ALSD_Jobsheet12/DoubleLinekdListMain08.java
@@ -46,5 +46,28 @@ public class DoubleLinekdListMain08 {
         dll.remove(1);
         dll.print();
         System.out.println("Size : " + dll.size());
+        dll.removeFirst();
+
+
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("================================");
+
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("================================");
+
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("================================");
+
+        System.out.println("Data awal pada Linked Lists adalah: " + dll.getFirst());
+        System.out.println("Data akhir pada Linked Lists adalah: " + dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked Lists adalah: " + dll.get(1));
+
     }
 }
--- src/Semester2/Pertemuan13ALSD_Jobsheet12/DoubleLinkedList08.java
+++ src/Semester2/Pertemuan13ALSD_Jobsheet12/DoubleLinkedList08.java
@@ -115,7 +115,7 @@ public class DoubleLinkedList08 {
         current.next = null;
         size--;
     }
-    
+
     public void remove(int index) throws Exception {
         if (isEmpty() || index >= size) {
             throw new Exception("Nilai indeks di luar batas");
@@ -140,5 +140,34 @@ public class DoubleLinkedList08 {
             }
             size--;
         }
+    }
+
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List kosong");
+        }
+        return head.data;
+    }
+
+    public int getLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List kosong");
+        }
+        Node08 tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas.");
+        }
+        Node08 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
     }
 }
